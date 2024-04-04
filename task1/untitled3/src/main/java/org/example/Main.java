6cecd1ac5e5ab246fa2c7f59fe4eaa638669b68e
Prudnik Dzmitry <dzmitry.prudnik@innowise.com>
2024-04-04 12:13:04 +0300

    Forgot to add Main method

---
+++ task1/untitled3/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.service.UserInputService;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInputService service = new UserInputService();
+        service.runUserInputs();
+    }
+}
